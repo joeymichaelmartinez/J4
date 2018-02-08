994ee700dae6cc5eafbf68555f243e5a2dd3ba85
track394 <track394@yahoo.com>
2018-02-07 22:53:21 -0800

    Got Mocha installed and working.

---
+++ test/run_j4_tests.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+describe('The simple dry run test', function() {
+    it('should pass because I told it to', function() {
+      assert.equal(true, true);
+    });
+});
