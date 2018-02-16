dc0ebc207ae6a06cadec8cb430de1af4f231ce02
track394 <track394@yahoo.com>
2018-02-15 19:36:25 -0800

    Started fixing parser

--- syntax/parser.js
+++ syntax/parser.js
@@ -1,94 +1,58 @@
 /*
- * Parser module
+ * Preparser module
  *
- *   const parse = require('./parser');
+ *   const withIndentsAndDedents = require('./preparser');
  *
- *   parse(text)
- *       Returns the abstract syntax tree for the given program text. This
- *       function will first pre-parse (figure out indents and dedents),
- *       then match against an Ohm grammar, then apply AST generation
- *       rules. If there are any errors, this function will throw an error.
+ *   withIndentsAndDedents(source)
+ *       Returns text with ⇨ characters replacing indent regions and ⇦
+ *       replacing dedents. If the source text is misindented, or has
+ *       any whitespace other than a space character in the indentation
+ *       region, the function will throw an error.
+ *
+ *   That's right: the only whitespace character allowed for indenting is
+ *   the plain old space character, \x20. All other whitespace, including
+ *   tabs, are illegal.
  */
 
-const fs = require('fs');
-const ohm = require('ohm-js');
-const withIndentsAndDedents = require('./preparser');
-/*
-const Program = require('../ast/program');
-const VariableDeclaration = require('../ast/variable-declaration');
-const AssignmentStatement = require('../ast/assignment-statement');
-const BreakStatement = require('../ast/break-statement');
-const ReturnStatement = require('../ast/return-statement');
-const IfStatement = require('../ast/if-statement');
-const Case = require('../ast/case');
-const WhileStatement = require('../ast/while-statement');
-const CallStatement = require('../ast/call-statement');
-const FunctionDeclaration = require('../ast/function-declaration');
-const BinaryExpression = require('../ast/binary-expression');
-const UnaryExpression = require('../ast/unary-expression');
-const IdentifierExpression = require('../ast/identifier-expression');
-const SubscriptedExpression = require('../ast/subscripted-expression');
-const Call = require('../ast/call');
-const Parameter = require('../ast/parameter');
-const Argument = require('../ast/argument');
-const BooleanLiteral = require('../ast/boolean-literal');
-const NumericLiteral = require('../ast/numeric-literal');
-const StringLiteral = require('../ast/string-literal');
-*/
-const grammar = ohm.grammar(fs.readFileSync('./syntax/J4Grammar.ohm'));
+module.exports = (source) => {
+  const stack = [0];
+  let result = '';
+  const text = source.endsWith('\n') ? source : `${source}\n`;
+  const linePattern = /( *)(([^\n]*\n) | ([^\n]*\r\n))/g;//Finally windows friendly
 
-// Ohm turns `x?` into either [x] or [], which we should clean up for our AST.
-function unpack(a) {
-  return a.length === 0 ? null : a[0];
-}
+  for (let match = linePattern.exec(text); match !== null; match = linePattern.exec(text)) {
+    const [indent, content] = [match[1].length, match[2]];
 
-/* eslint-disable no-unused-vars *//*
-const astGenerator = grammar.createSemantics().addOperation('ast', {
-  Program(_1, body, _2) { return new Program(body.ast()); },
-  Stmt_simple(statement, _) { return statement.ast(); },
-  Stmt_while(_, test, suite) { return new WhileStatement(test.ast(), suite.ast()); },
-  Stmt_if(_1, firstTest, firstSuite, _2, moreTests, moreSuites, _3, lastSuite) {
-    const tests = [firstTest.ast(), ...moreTests.ast()];
-    const bodies = [firstSuite.ast(), ...moreSuites.ast()];
-    const cases = tests.map((test, index) => new Case(test, bodies[index]));
-    return new IfStatement(cases, unpack(lastSuite.ast()));
-  },
-  Stmt_def(_1, id, _2, params, _3, suite) {
-    return new FunctionDeclaration(id.ast(), params.ast(), suite.ast());
-  },
-  SimpleStmt_vardecl(_1, v, _2, e) { return new VariableDeclaration(v.ast(), e.ast()); },
-  SimpleStmt_assign(v, _, e) { return new AssignmentStatement(v.ast(), e.ast()); },
-  SimpleStmt_break(_) { return new BreakStatement(); },
-  SimpleStmt_return(_, e) { return new ReturnStatement(unpack(e.ast())); },
-  SimpleStmt_call(c) { return new CallStatement(c.ast()); },
-  Suite_small(_1, statement, _2) { return [statement.ast()]; },
-  Suite_large(_1, _2, _3, statements, _4) { return statements.ast(); },
-  Exp_or(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp_and(left, op, right) { return new BinaryExpression(op.ast(), right.ast()); },
-  Exp1_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp2_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp3_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp4_unary(op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
-  Exp5_parens(_1, expression, _2) { return expression.ast(); },
-  Call(callee, _1, args, _2) { return new Call(callee.ast(), args.ast()); },
-  VarExp_subscripted(v, _1, e, _2) { return new SubscriptedExpression(v.ast(), e.ast()); },
-  VarExp_simple(id) { return new IdentifierExpression(id.ast()); },
-  Param(id, _, exp) { return new Parameter(id.ast(), unpack(exp.ast())); },
-  Arg(id, _, exp) { return new Argument(unpack(id.ast()), exp.ast()); },
-  NonemptyListOf(first, _, rest) { return [first.ast(), ...rest.ast()]; },
-  EmptyListOf() { return []; },
-  boollit(_) { return new BooleanLiteral(!!this.sourceString); },
-  numlit(_1, _2, _3, _4, _5, _6) { return new NumericLiteral(+this.sourceString); },
-  strlit(_1, chars, _6) { return new StringLiteral(this.sourceString); },
-  id(_1, _2) { return this.sourceString; },
-  _terminal() { return this.sourceString; },
-});
-/* eslint-enable no-unused-vars */
-
-module.exports = (text) => {
-  const match = grammar.match(withIndentsAndDedents(text).replace(/(?:\r\n|\r|\n)/g, ';'));
-  if (!match.succeeded()) {
-    throw new Error(`Syntax Error: ${match.message}`);
+    if (content === '\n' || content === '\r\n') {//Windows friendly
+      // Empty line
+      result += content;
+    } else if (/\s/.test(content[0])) {
+      // First non-space character is whitespace, may be confusing so error
+      throw new Error(`Illegal whitespace character: \\u{${content.charCodeAt(0).toString(16)}}`);
+    } else if (indent === stack[stack.length - 1]) {
+      // This line is on the same indent level as the last line, proceed
+      result += content;
+    } else if (indent > stack[stack.length - 1]) {
+      // This line has moved in, so push the new level and emit an indent token
+      stack.push(indent);
+      result += `⇨${content}`;
+    } else {
+      // We've come out, so keep popping and emitting dedent tokens until done or error
+      for (let dedents = 1; true; dedents += 1) {
+        const next = (stack.pop(), stack[stack.length - 1]);
+        if (indent > next) {
+          throw new Error('Indent Error');
+        } else if (indent === next) {
+          result += `${'⇦'.repeat(dedents)}${content}`;
+          break;
+        }
+      }
+    }
   }
-  return match;
-};
+
+  // Add in any required dedents at the end
+  if (stack.length > 1) {
+    result += `${'⇦'.repeat(stack.length - 1)}\n`;
+  }
+  return result;
+}
