b650d430fae8bbb4b27d6566ac9472d550b3b325
track394 <track394@yahoo.com>
2018-04-14 16:40:44 -0700

    Added booltype and boollit objects

--- ast/boolean-literal.js
+++ ast/boolean-literal.js
@@ -1,5 +1,12 @@
+const BoolType = require("./bool-type");
+
 module.exports = class BooleanLiteral {
     constructor(value) {
         this.value = value;
+        this.type = new BoolType;
+    }
+
+    analyze() {
+        //Let empty on purpose
     }
 };
