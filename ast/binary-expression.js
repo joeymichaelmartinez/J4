b3afa34c6637312a6cd6b1d3bc62771e324d0718
track394 <track394@yahoo.com>
2018-04-25 20:26:32 -0700

    J and I got binary expressions started

--- ast/binary-expression.js
+++ ast/binary-expression.js
@@ -6,6 +6,22 @@ module.exports = class BinaryExpression {
     analyze(context) {
         this.left.analyze(context);
         this.right.analyze(context);
+        let leftType;
+        let rightType;
+        if (this.left.id) {
+            leftType = this.left.referent.type;
+        } else {
+            leftType = this.left.type;
+        }
+        if (this.right.id) {
+            rightType = this.right.referent.type;
+        } else {
+            rightType = this.right.type;
+        }
+        if (leftType.toString() !== rightType.toString()) {
+            throw new Error("type of left expression does not match type of right expression");
+        }
+        this.type = leftType;
     }
 
     optimize() {
