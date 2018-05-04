85bec0ee59544f38f023b5aa6cf977ac2562ec3d
JacksonJW <jacksonjwatkins@gmail.com>
2018-05-04 00:24:05 -0700

    Added some more to the test suite for the generator

--- test/generator/generator-test.js
+++ test/generator/generator-test.js
@@ -7,14 +7,20 @@ const TEST_DIR = "./test/generator/";
 
 describe("The generator", () => {
     fs.readdirSync(TEST_DIR).forEach((name) => {
-        if (name.endsWith(".error")) {
-            it(`detects a syntax error in ${name}`, (done) => {
-                fs.readFile(`${TEST_DIR}/${name}`, "utf-8", (err, input) => {
-                    // We always wrap Ohm failures in an error with text "Syntax Error"
-
-                    assert.throws(() => parse(input), /Syntax Error/);
-                    done();
-                });
+        if (name.endsWith(".j4")) {
+            it(`verifies that the expected output of ${name}.j4 equals the actual ouput ${name}.`, (done) => {
+                // We always wrap Ohm failures in an error with text "Syntax Error"
+                const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
+                program.analyze();
+                let expectedOutput = eval(program.gen());
+                assert.deepEqual(expectedOutput, );
+                // assert.deepEqual();
+                // assert.throws(() => program.analyze(), errorPattern);
+                // fs.readFile(`${__dirname}/${name}.json`, "utf-8", (_err, expected) => {
+                //     assert.deepEqual(ast, JSON.parse(expected));
+                //     done();
+                // });
+                done();
             });
         }
     });
