cc2be3104fd7a7d51cd43e6dabfcb6ba0340b234
JacksonJW <jacksonjwatkins@gmail.com>
2018-05-04 00:39:14 -0700

    fixed punctuation, close to finishing generator suite

--- test/generator/generator-test.js
+++ test/generator/generator-test.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const assert = require("assert");
-const generator = require("javascript-generator.js");
+const generator = require("../../backend/javascript-generator.js");
 const mainFile = require("../../j4.js");
 const parse = require("../../../syntax/parser");
 const TEST_DIR = "../test/generator/";
@@ -9,7 +9,6 @@ describe("The generator", () => {
     fs.readdirSync(TEST_DIR).forEach((name) => {
         if (name.endsWith(".j4")) {
             it(`verifies that the expected output of ${name}.j4 equals the actual ouput which is in ${name}.txt`, (done) => {
-                // We always wrap Ohm failures in an error with text "Syntax Error"
                 const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
                 program.analyze();
                 let expectedOutput = eval(program.gen());
