7b9be66e884f8be2672ba3f9cdb1189d336faa06
JacksonJW <jacksonjwatkins@gmail.com>
2018-05-04 00:35:01 -0700

    Added generator test suite to the test folder but the npm test is not recognizing it

--- test/generator/generator-test.js
+++ test/generator/generator-test.js
@@ -3,17 +3,17 @@ const assert = require("assert");
 const generator = require("javascript-generator.js");
 const mainFile = require("../../j4.js");
 const parse = require("../../../syntax/parser");
-const TEST_DIR = "./test/generator/";
+const TEST_DIR = "../test/generator/";
 
 describe("The generator", () => {
     fs.readdirSync(TEST_DIR).forEach((name) => {
         if (name.endsWith(".j4")) {
-            it(`verifies that the expected output of ${name}.j4 equals the actual ouput ${name}.`, (done) => {
+            it(`verifies that the expected output of ${name}.j4 equals the actual ouput which is in ${name}.txt`, (done) => {
                 // We always wrap Ohm failures in an error with text "Syntax Error"
                 const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
                 program.analyze();
                 let expectedOutput = eval(program.gen());
-                assert.deepEqual(expectedOutput, );
+                assert.deepEqual(expectedOutput, fs.readFileSync(`${__dirname}/${name}.txt`, "utf8"));
                 // assert.deepEqual();
                 // assert.throws(() => program.analyze(), errorPattern);
                 // fs.readFile(`${__dirname}/${name}.json`, "utf-8", (_err, expected) => {
